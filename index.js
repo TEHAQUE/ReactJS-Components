8114b7c3c964893e9ecd60b21835262c161b85d6
TEHAQUE <82551359+TEHAQUE@users.noreply.github.com>
2023-04-11 22:31:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Parallax from './parallax/jsx/Parallax';
+import Slider from './slider with timer/jsx/slider';
+
+function Parallaxv1() {
+   return <Parallax />
+}
+function Sliderv1() {
+   return <Slider />
+}
+
+modeule.export = {Parallaxv1,Sliderv1};
