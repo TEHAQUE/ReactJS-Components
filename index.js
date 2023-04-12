5ce94aeced22c02dc904f511c8608780984f4e91
TEHAQUE <82551359+TEHAQUE@users.noreply.github.com>
2023-04-12 04:55:12 +0200

    Update and rename dist/index.js to index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ Object.defineProperty(exports, "Slider", {
   }
 });
 var _react = _interopRequireDefault(require("react"));
-var _Parallax = _interopRequireDefault(require("./parallax/Parallax"));
-var _slider = _interopRequireDefault(require("./slider_with_timer/slider"));
+var _Parallax = _interopRequireDefault(require(".dist/parallax/Parallax"));
+var _slider = _interopRequireDefault(require("./dist/slider_with_timer/slider"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
