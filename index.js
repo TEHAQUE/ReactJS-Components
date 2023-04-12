079029095f79ebd491a3b28cb1178d4d95b50eac
TEHAQUE <82551359+TEHAQUE@users.noreply.github.com>
2023-04-12 04:28:00 +0200

    v1.0.8 - experimental

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Parallax from './parallax/jsx/Parallax';
-import Slider from './slider with timer/jsx/slider';
+"use strict";
 
-function Parallaxv1() {
-   return <Parallax />
-}
-function Sliderv1() {
-   return <Slider />
-}
-
-modeule.export = {Parallaxv1,Sliderv1};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Parallax", {
+  enumerable: true,
+  get: function get() {
+    return _Parallax["default"];
+  }
+});
+Object.defineProperty(exports, "Slider", {
+  enumerable: true,
+  get: function get() {
+    return _slider["default"];
+  }
+});
+var _Parallax = _interopRequireDefault(require("./dist/parallax/Parallax"));
+var _slider = _interopRequireDefault(require("./dist/slider_with_timer/slider"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
