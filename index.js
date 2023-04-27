131c56015066ea4bc32bf2253a590ee99da7068a
TEHAQUE <82551359+TEHAQUE@users.noreply.github.com>
2023-04-27 21:20:38 +0200

    v1.0.17

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "MainMenu", {
+  enumerable: true,
+  get: function get() {
+    return _mainMenu["default"];
+  }
+});
 Object.defineProperty(exports, "Parallax", {
   enumerable: true,
   get: function get() {
@@ -16,6 +22,7 @@ Object.defineProperty(exports, "Slider", {
   }
 });
 var _react = _interopRequireDefault(require("react"));
-var _Parallax = _interopRequireDefault(require("./dist/parallax/Parallax"));
-var _slider = _interopRequireDefault(require("./dist/slider_with_timer/slider"));
+var _Parallax = _interopRequireDefault(require("./parallax/Parallax"));
+var _slider = _interopRequireDefault(require("./slider_with_timer/slider"));
+var _mainMenu = _interopRequireDefault(require("./main_menu/mainMenu"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
