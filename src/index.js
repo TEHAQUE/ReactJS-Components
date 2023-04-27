03c05cddf6ce37ad509aaa3bb8ba06aaa7c1500a
TEHAQUE <82551359+TEHAQUE@users.noreply.github.com>
2023-04-27 21:19:15 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Parallax from './parallax/Parallax';
-import Slider from './slider_with_timer/slider';
-import MainMenu from './main_menu/mainMenu';
-
-export {Parallax,Slider,MainMenu};
